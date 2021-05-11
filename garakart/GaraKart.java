a4bafc223198e49ae625e9db81340f5bda3bf3a6
mirco morano <morano.mirco@gmail.com>
2021-05-11 08:12:17 +0200

    pronto

---
+++ garakart/GaraKart.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package garakart;
+
+/**
+ *
+ * @author 39334
+ */
+public class GaraKart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Kart k1=new Kart();
+        Kart k2=new Kart();
+        Kart k3=new Kart();
+        Kart k4=new Kart();
+        
+        Spogliatoio s1=new Spogliatoio();
+        Spogliatoio s2=new Spogliatoio();
+        
+        Pilota p1=new Pilota();
+        Pilota p2=new Pilota();
+        Pilota p3=new Pilota();
+        Pilota p4=new Pilota();
+        Pilota p5=new Pilota();
+        Pilota p6=new Pilota();
+        Pilota p7=new Pilota();
+        Pilota p8=new Pilota();
+    }
+    
+}
